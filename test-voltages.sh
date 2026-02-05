66cf2d86b750128477aa79bdd5f7db2b6d4a5581
chaslinux <chaslinux@gmail.com>
2026-02-05 11:49:00 -0500

    Initial commit to test voltages on potentially multiple batteries.

---
+++ test-voltages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get all battery paths
+BPATHS=($(upower -e | grep BAT))
+
+# Initialize voltage array
+voltages=()
+
+# Loop over batteries and get voltages
+for bat in "${BPATHS[@]}"; do
+    voltage=$(upower -i "$bat" | grep 'voltage:' | awk '{print $2 " " $3}')
+    voltages+=("$voltage")
+done
+
+# Print all voltages
+for i in "${!voltages[@]}"; do
+    echo "Battery $i voltage: ${voltages[$i]}"
+done
+
