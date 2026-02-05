7ecb6cf2a1723fba60e4d2193d0aaf5e973be9b2
chaslinux <chaslinux@gmail.com>
2026-02-05 12:28:49 -0500

    Change to only show the voltage values.

--- test-voltages.sh
+++ test-voltages.sh
@@ -8,7 +8,8 @@ voltages=()
 
 # Loop over batteries and get voltages
 for bat in "${BPATHS[@]}"; do
-    voltage=$(upower -i "$bat" | grep 'voltage:' | awk '{print $2 " " $3}')
+    # Extract numeric voltage only
+    voltage=$(upower -i "$bat" | grep 'voltage:' | awk '{print $2}')
     voltages+=("$voltage")
 done
 
@@ -16,4 +17,5 @@ done
 for i in "${!voltages[@]}"; do
     echo "Battery $i voltage: ${voltages[$i]}"
 done
+
 
